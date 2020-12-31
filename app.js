c02f056bea4cd03f5db419cebb12dd9b346e76c6
Arno Solo <46125820+arnosolo@users.noreply.github.com>
2020-12-31 18:10:19 +0800

    Organize comments

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const grid = document.querySelector('.grid')
-    let squares = Array.from(document.querySelectorAll('.grid div'))
-    let scoreDisplay = document.querySelector('#score')
-    let score = scoreDisplay.innerHTML*1
-    const width = 10 // 一行有几个格子
-    let gameSpeed = 450
-
+    // some userPreference
+    let userPreference = {
+        gameSpeed: 450
+    }
     const color = [
         'lightgray',
         'lightskyblue',
@@ -14,13 +11,20 @@ document.addEventListener("DOMContentLoaded", () => {
         'cornflowerblue',
         'chocolate',
     ]
+
+    let scoreDisplay = document.querySelector('#score')
+    let score = scoreDisplay.innerHTML*1
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const width = 10 // block num per row
+    let gameIsRunning = false
+    let timerId = null
     
-    // 积木所有形态
+    // def shapes of tetrominoes
     const lTetromino = [
         [1, width+1, width*2+1, 2],
         [width+0, width+1, width+2, width*2+2],
         [width*2+0, 1, width+1, width*2+1],
-        // [width+0, width*2+0, width*2+1, width*2+2],
         [width+0, width*2+0, width*2+1, width*2+2]
     ]
     
@@ -54,23 +58,23 @@ document.addEventListener("DOMContentLoaded", () => {
     
     const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
     
-    // 定义 预览下一个
+    // def shapes of upNextTetromino
     const previewSquares = Array.from(document.querySelectorAll('.mini-grid div'))
     const previewWidth = 4
     const upNextTetromino = [
         [1, previewWidth+1, previewWidth*2+1, 2], // L
-        [0, previewWidth,previewWidth+1,previewWidth*2+1], // Z
-        [1, previewWidth,previewWidth+1,previewWidth+2], // T
-        [0, 1, previewWidth,previewWidth+1], // O
-        [1, previewWidth+1,previewWidth*2+1,previewWidth*3+1], // I
+        [0, previewWidth, previewWidth+1, previewWidth*2+1], // Z
+        [1, previewWidth, previewWidth+1, previewWidth+2], // T
+        [0, 1, previewWidth, previewWidth+1], // O
+        [1, previewWidth+1, previewWidth*2+1, previewWidth*3+1], // I
 
     ]
     function drawPreview(typeIndex) {
-        // 清除上一次预览
+        // clean last
         previewSquares.forEach(square => {
             square.classList.remove('tetromino')
             square.style.backgroundColor = ''
-        }) 
+        })
         upNextTetromino[typeIndex].forEach(index => {
             previewSquares[index].classList.add('tetromino')
             previewSquares[index].style.backgroundColor = color[nextRandom]
@@ -78,15 +82,17 @@ document.addEventListener("DOMContentLoaded", () => {
         return typeIndex
     }
 
+
+    // game init
     let nextRandom = Math.floor(Math.random() * theTetrominoes.length)
     let random = Math.floor(Math.random() * theTetrominoes.length)
     drawPreview(nextRandom)
     let currentPosition = 4
     let currentRotation = 0
-    // 正在行进的 tetromino
+    // theTetrominoes[0][0] === [1, width+1, width*2+1, 2]
     let current = theTetrominoes[random][currentRotation]
 
-    // 定义 如何绘制tetromino
+    // def how to draw tetromino
     function draw() {
         current.forEach(i => {
             squares[currentPosition + i].classList.add('tetromino')
@@ -94,16 +100,15 @@ document.addEventListener("DOMContentLoaded", () => {
         })
     }
 
-    // 定义 如何擦除tetromino
+    // def how to undraw tetromino
     function undraw() {
         current.forEach(index => {
             squares[currentPosition + index].classList.remove('tetromino')
             squares[currentPosition + index].style.backgroundColor = ''
-
         })
     }
 
-    // 定义 tetromino将如何坠落
+    // def how tetromino fall
     function moveDown(params) {
         undraw()
         currentPosition += width
@@ -111,29 +116,33 @@ document.addEventListener("DOMContentLoaded", () => {
         freeze()
     }
     
-    // 定义 何时不再坠落
+    // def when tetromino stop falling, what should be done at that time.
     function freeze(params) {
-        // 如果下一行有被 taken, 
+        // if some squares of next row are taken
         if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
             current.forEach(index => squares[currentPosition + index].classList.add('taken'))
-            // 新的tetromino开始下落
+            
+            // preview tetromino --> current tetromino
             random = nextRandom
+            // random tetromino --> preview tetromino
             nextRandom = Math.floor(Math.random() * theTetrominoes.length)
             current = theTetrominoes[random][currentRotation]
             currentPosition = 4
+
+            // render the page
             draw()
             drawPreview(nextRandom)
             addScore()
+
+
             isGameOver()
         }
     }
     
-    // tetromino向下坠落
-    let gameIsRunning = false // false为未进行,ture为正在进行
-    let timerId = null
+    // start-button
     function start() {
         gameIsRunning = true
-        timerId = setInterval(moveDown, gameSpeed)
+        timerId = setInterval(moveDown, userPreference.gameSpeed)
     }
     function pause() {
         gameIsRunning = false
@@ -144,15 +153,13 @@ document.addEventListener("DOMContentLoaded", () => {
         gameIsRunning ? pause() : start()
     })
 
-    // 定义 向左移动
     function moveLeft() {
         undraw()
         
-        // 如果没到最左边,左移一格
         const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
         if(!isAtLeftEdge) currentPosition -= 1
 
-        // 如果左边已经占用, 恢复原位
+        // if hit left border, go back
         if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
             currentPosition += 1
         }
@@ -160,15 +167,12 @@ document.addEventListener("DOMContentLoaded", () => {
         draw()
     }
 
-    // 定义 向右移动
     function moveRight() {
         undraw()
         
-        // 如果没到最右边,右移一格
         const isRightEdge = current.some(index => (currentPosition + index) % width === width-1)
         if(!isRightEdge) currentPosition += 1
 
-        // 如果右边已经占用, 恢复原位
         if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
             currentPosition -= 1
         }
@@ -176,7 +180,7 @@ document.addEventListener("DOMContentLoaded", () => {
         draw()
     }
 
-    // 定义 转换状态
+    // def how tetromino rotate
     function rotate() {
         undraw()
         currentRotation ++
@@ -187,7 +191,7 @@ document.addEventListener("DOMContentLoaded", () => {
         draw()
     }
 
-    // 定义 按键控制
+    // def keyboard instructions
     function control(e) {
         // console.log(e.keyCode);
         if(gameIsRunning === false) {
@@ -212,18 +216,22 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     document.addEventListener('keyup', control)
 
-    // 定义 消除行并加分
+    // def how to addscore and clean scored rows
     function addScore() {
+        // scan each row
         for(let i=0; i < 199; i +=width){
             const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
+            // check is current row should be scored
             if(row.every(value => squares[value].classList.contains('taken'))){
                 score += 10
                 scoreDisplay.innerHTML = score
+                // clean the style of scored row
                 row.forEach(value => {
                     squares[value].classList.remove('taken')
                     squares[value].classList.remove('tetromino')
                     squares[value].style.backgroundColor = ''
                 })
+                // move scored row to 1st row of the grid
                 let removedSquares = squares.splice(i, width)
                 squares = removedSquares.concat(squares)
                 squares.forEach(square => grid.appendChild(square))
@@ -231,12 +239,12 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
     
-    // 定义 游戏结束
+    // isGameOver
     function isGameOver() {
         if(current.some(value => squares[currentPosition + value].classList.contains('taken'))){
             gameIsRunning = false
             clearInterval(timerId)
-            scoreDisplay.innerHTML = `游戏结束,得分为${score}.`
+            scoreDisplay.innerHTML = `Game Over, your score is ${score}.`
         }
     }
 
